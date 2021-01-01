0106fbb36b54d05e9c1000140dd1990286f7e8a0
vision-rsl <rsladke99@gmail.com>
2021-01-01 21:43:47 +0530

    Global Chat

---
+++ build/precache-manifest.8e83df135bc1737f308dbaa8a86775c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b9aca6143e9ef982d696dad09ffe8c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a07bb6b5fa0459ae00fb",
+    "url": "/static/css/main.736eea2c.chunk.css"
+  },
+  {
+    "revision": "cd23ec4521fcc5122634",
+    "url": "/static/js/2.a79127c2.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/static/js/2.a79127c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a07bb6b5fa0459ae00fb",
+    "url": "/static/js/main.e2e4c140.chunk.js"
+  },
+  {
+    "revision": "65eb99d39abb7be77a81",
+    "url": "/static/js/runtime-main.f15aa52d.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,128 @@
+import React, { useRef, useState } from 'react';
+import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  //your config
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+function App() {
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Global Chat 👋</h1>
+        <SignOut />
+      </header>
+
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
+    </div>
+  );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>
+        Sign in with Google
+      </button>
+      {/* <p>
+        Do not violate the community guidelines or you will be banned for life!
+      </p> */}
+    </>
+  );
+}
+
+function SignOut() {
+  return (
+    auth.currentUser && (
+      <a className="sign-out" onClick={() => auth.signOut()}>
+        <img src="https://cdn.iconscout.com/icon/premium/png-256-thumb/signout-1-561714.png" />
+      </a>
+    )
+  );
+}
+
+function ChatRoom() {
+  const dummy = useRef();
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(1000);
+
+  const [messages] = useCollectionData(query, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL,
+    });
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  return (
+    <>
+      <main>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+
+        <span ref={dummy}></span>
+      </main>
+
+      <form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+          placeholder="HappyNewYear2021"
+        />
+
+        <button type="submit" disabled={!formValue}>
+          📡
+        </button>
+      </form>
+    </>
+  );
+}
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (
+    <>
+      <div className={`message ${messageClass}`}>
+        <img
+          src={photoURL || 'https://www.w3schools.com/w3images/avatar6.png'}
+        />
+        <p>{text}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
